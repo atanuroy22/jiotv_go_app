6389c76966e429b6a545449fe80cd53fd706bdaf
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-09-23 00:36:25 +0530

    Disabled auto IPTV on boot

--- app/src/main/java/com/skylake/skytv/jgorunner/receivers/BootReceiver.java
+++ app/src/main/java/com/skylake/skytv/jgorunner/receivers/BootReceiver.java
@@ -42,58 +42,57 @@ public class BootReceiver extends BroadcastReceiver {
                 }
                 Log.d("BootReceiver", "BinaryService started in the background.");
 
-                if (isAutobootIPTV != null && isAutobootIPTV.equals("Yes")) {
-                    Log.d("work", "ofART");
-                    executor.execute(() -> {
-                        try {
-                            boolean isServerRunning = checkServerStatus();
-
-                            if (isServerRunning) {
-                                String appPackageName = preferenceManager.getKey("app_packagename");
-                                if (!appPackageName.isEmpty()) {
-                                    Log.d("DIX", appPackageName);
-                                    String appLaunchActivity = preferenceManager.getKey("app_launch_activity");
-                                    String appName = preferenceManager.getKey("app_name");
-
-                                    PackageManager packageManager = context.getPackageManager();
-
-                                    if ("webtv".equals(appPackageName)) {
-                                        Intent launchIntent = new Intent(context, WebPlayerActivity.class);
-                                        launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        context.startActivity(launchIntent);
-                                        Toast.makeText(context, "Opening IPTV", Toast.LENGTH_SHORT).show();
-                                        Log.d("DIX", "Opening IPTV");
-                                    } else if (!appPackageName.isEmpty() && !appName.isEmpty()) {
-                                        Intent launchIntent = packageManager.getLaunchIntentForPackage(appPackageName);
-                                        if (launchIntent != null) {
-                                            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                            context.startActivity(launchIntent);
-                                            Toast.makeText(context, "Opening " + appName, Toast.LENGTH_SHORT).show();
-                                            Log.d("DIX", "Opening " + appName);
-                                        } else {
-                                            Toast.makeText(context, "Cannot find the specified application", Toast.LENGTH_SHORT).show();
-                                            Log.d("DIX", "Cannot find the specified application");
-                                        }
-                                    } else {
-                                        Toast.makeText(context, "No application details found", Toast.LENGTH_SHORT).show();
-                                        Log.d("DIX", "No application details found");
-                                    }
-                                } else {
-                                    Toast.makeText(context, "IPTV not set.", Toast.LENGTH_SHORT).show();
-                                    Log.d("DIX", "IPTV not set");
-                                }
-                            } else {
-                                Toast.makeText(context, "Server issue. If it persists, restart.", Toast.LENGTH_SHORT).show();
-                                Log.d("DIX", "Server issue. If it persists, restart");
-                            }
-                        } catch (Exception e) {
-                            Log.e("DIX", "Error starting IPTV", e);
-                            Toast.makeText(context, "Error starting IPTV", Toast.LENGTH_SHORT).show();
-                            Log.d("DIX", "Error starting IPTV: " + e.getMessage());
-                        }
-
-                    });
-                }
+//                if (isAutobootIPTV != null && isAutobootIPTV.equals("Yes")) {
+//                    executor.execute(() -> {
+//                        try {
+//                            boolean isServerRunning = checkServerStatus();
+//
+//                            if (isServerRunning) {
+//                                String appPackageName = preferenceManager.getKey("app_packagename");
+//                                if (!appPackageName.isEmpty()) {
+//                                    Log.d("DIX", appPackageName);
+//                                    String appLaunchActivity = preferenceManager.getKey("app_launch_activity");
+//                                    String appName = preferenceManager.getKey("app_name");
+//
+//                                    PackageManager packageManager = context.getPackageManager();
+//
+//                                    if ("webtv".equals(appPackageName)) {
+//                                        Intent launchIntent = new Intent(context, WebPlayerActivity.class);
+//                                        launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                                        context.startActivity(launchIntent);
+//                                        Toast.makeText(context, "Opening IPTV", Toast.LENGTH_SHORT).show();
+//                                        Log.d("DIX", "Opening IPTV");
+//                                    } else if (!appPackageName.isEmpty() && !appName.isEmpty()) {
+//                                        Intent launchIntent = packageManager.getLaunchIntentForPackage(appPackageName);
+//                                        if (launchIntent != null) {
+//                                            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                                            context.startActivity(launchIntent);
+//                                            Toast.makeText(context, "Opening " + appName, Toast.LENGTH_SHORT).show();
+//                                            Log.d("DIX", "Opening " + appName);
+//                                        } else {
+//                                            Toast.makeText(context, "Cannot find the specified application", Toast.LENGTH_SHORT).show();
+//                                            Log.d("DIX", "Cannot find the specified application");
+//                                        }
+//                                    } else {
+//                                        Toast.makeText(context, "No application details found", Toast.LENGTH_SHORT).show();
+//                                        Log.d("DIX", "No application details found");
+//                                    }
+//                                } else {
+//                                    Toast.makeText(context, "IPTV not set.", Toast.LENGTH_SHORT).show();
+//                                    Log.d("DIX", "IPTV not set");
+//                                }
+//                            } else {
+//                                Toast.makeText(context, "Server issue. If it persists, restart.", Toast.LENGTH_SHORT).show();
+//                                Log.d("DIX", "Server issue. If it persists, restart");
+//                            }
+//                        } catch (Exception e) {
+//                            Log.e("DIX", "Error starting IPTV", e);
+//                            Toast.makeText(context, "Error starting IPTV", Toast.LENGTH_SHORT).show();
+//                            Log.d("DIX", "Error starting IPTV: " + e.getMessage());
+//                        }
+//
+//                    });
+//                }
             }
         }
     }
