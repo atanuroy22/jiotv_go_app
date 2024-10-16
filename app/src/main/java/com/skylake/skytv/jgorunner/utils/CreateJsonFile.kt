1684e28b6d42ac58f28fa7c396add1a282eef93e
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-10-17 00:47:29 +0530

    Update CreateJsonFile.kt

--- app/src/main/java/com/skylake/skytv/jgorunner/utils/CreateJsonFile.kt
+++ app/src/main/java/com/skylake/skytv/jgorunner/utils/CreateJsonFile.kt
@@ -1,13 +1,33 @@
 package com.skylake.skytv.jgorunner.utils
 
 import android.content.Context
+import android.os.Environment
 import android.util.Log
 import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStreamWriter
 
-fun createJsonFile(context: Context, fileName: String, jsonData: String) {
-    val file = File(context.filesDir, fileName)
+fun createJsonFile(context: Context, fileName: String, jsonData: String, useExternalStorage: Boolean) {
+    val file: File
+
+    if (useExternalStorage) {
+        // Get external storage directory
+        val externalStoragePath = Environment.getExternalStorageDirectory().path
+        val folder = File(externalStoragePath, ".jiotv_go")
+
+        // Create the folder if it doesn't exist
+        if (!folder.exists()) {
+            if (!folder.mkdirs()) {
+                Log.e("FileCreation", "Failed to create directory: ${folder.absolutePath}")
+                return
+            }
+        }
+
+        file = File(folder, fileName)
+    } else {
+        // Internal storage
+        file = File(context.filesDir, fileName)
+    }
 
     // Check if the file already exists
     if (file.exists()) {
@@ -21,7 +41,7 @@ fun createJsonFile(context: Context, fileName: String, jsonData: String) {
                 writer.write(jsonData)
             }
         }
-        Log.d("FileCreation", "File created and data written successfully.")
+        Log.d("FileCreation", "File created and data written successfully: ${file.absolutePath}")
     } catch (e: Exception) {
         Log.e("FileCreation", "Error creating file: ${e.message}")
     }
