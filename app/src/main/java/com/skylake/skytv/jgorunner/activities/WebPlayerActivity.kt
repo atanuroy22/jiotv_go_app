a8de1428ca59ad6f104d005b37c9a5408d65a57f
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2025-01-22 01:24:45 +0530

    fix: webtv fullscreen issue

--- app/src/main/java/com/skylake/skytv/jgorunner/activities/WebPlayerActivity.kt
+++ app/src/main/java/com/skylake/skytv/jgorunner/activities/WebPlayerActivity.kt
@@ -136,18 +136,20 @@ class WebPlayerActivity : ComponentActivity() {
 
     private fun updateSystemUiVisibility() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
             window.insetsController?.let { controller ->
                 controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
                 controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
         } else {
             @Suppress("deprecation")
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
         }
     }
+
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun setupWebView() {
@@ -187,6 +189,7 @@ class WebPlayerActivity : ComponentActivity() {
     override fun onResume() {
         super.onResume()
         webView!!.onResume()
+//        webView!!.loadUrl(url!!)
     }
 
 
